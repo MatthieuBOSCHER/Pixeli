56ebaaf8d8842cfc08f21d1077a036af4c19ef73
Matthieu BOSCHER <matthieu.boscher@epitech.digital>
2022-05-02 23:51:22 +0200

    now working with argparse, taking resolution as arg

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from PIL import Image
-from sys import argv
+import argparse
 
-image_input_path = argv[1]
-image_output_path = argv[2]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--format", help="Format to be resized", default=(32, 32))
+
+parser.add_argument('paths', nargs='*')
+
+args = parser.parse_args()
+
+arg_format = args.format[1:-1]
+
+img_resolution = tuple(map(int, arg_format.split(', ')))
+
+image_input_path = f"./image_input/{args.paths[0]}"
+image_output_path = f"./image_output/{args.paths[1]}_{img_resolution[0]}x{img_resolution[1]}.png"
 
 img = Image.open(image_input_path)
 
-imgSmall = img.resize((16, 16), resample=Image.BILINEAR)
+imgSmall = img.resize(img_resolution, resample=Image.BILINEAR)
 
 result = imgSmall.resize(img.size, Image.NEAREST)
 
