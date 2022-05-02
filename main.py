078fee9256dfc58e0f9b171dde12d60c9c440b69
Matthieu BOSCHER <matthieu.boscher@epitech.digital>
2022-05-02 23:25:47 +0200

    base is working

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from PIL import image
-import argparse
+from PIL import Image
+from sys import argv
 
+image_input_path = argv[1]
+image_output_path = argv[2]
+
+img = Image.open(image_input_path)
+
+imgSmall = img.resize((16, 16), resample=Image.BILINEAR)
+
+result = imgSmall.resize(img.size, Image.NEAREST)
+
+result.save(image_output_path)
